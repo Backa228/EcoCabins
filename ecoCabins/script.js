ccbc15c5332c41eec7b1780183a4397824a2f631
Backa228 <andrey.scott01@gmail.com>
2024-10-07 18:31:57 +0300

    lesson03.10
    Хедер доробили, продовжили стілізувати свайпер, виправили декілька помилок.

--- ecoCabins/script.js
+++ ecoCabins/script.js
@@ -1,29 +1,30 @@
 document.addEventListener("DOMContentLoaded", function() {
     const swiper = new Swiper('.materials__slider', {
         direction: 'horizontal',
-        loop: true,
+        // loop: true,
         pagination: {
             el: '.swiper-pagination',
             clickable: true,
         },
     });
 });
-let lastScrollTop = 0;//змінна для збереження позиції останнього скролінгу
-const header = document.querySelector(".header");
 
-window.addEventListener('scroll', () => {
+let lastScrollTop = 0;
+const header = document.querySelector('.header');
 
+console.log(header);
+
+window.addEventListener ('scroll', () => {
     let scrollTop = window.scrollY || document.documentElement.scrollTop;
     if ((scrollTop > lastScrollTop) && (scrollTop > header.offsetHeight)) {
         header.classList.add('hidden');
         header.classList.add('dark');
     } else if (scrollTop < lastScrollTop) {
         header.classList.remove('hidden');
-  }
-  if (scrollTop < header.offsetHeight) {
-      header.classList.remove("dark")
+    } 
+    if (scrollTop < header.offsetHeight) {
+        header.classList.remove('dark');
     }
 
     lastScrollTop = scrollTop;
-
 });
